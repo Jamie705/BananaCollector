3185481d9259129cb2224113dfa622eb4281473f
myusername <rodriguez.jamie@live.com>
2018-09-05 21:48:57 -0400

    mostly functional, need to get reset to work, and add css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,97 @@
 // alert("something");
 
-// Creates list/array of abc's
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Creating variables to hold the number of wins, losses, guesses left and list of guessed letters.
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
-var userGuessed = [];
-var computerGuess= "";
 
-// Randomly math to choose letter in alphabet. Computer's guess. Created in funtion to reset word.
-function newLetter() {
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log ("Computer guessed: " + computerGuess);  
-    }
+    // Randomly math to choose a number from 19-120
+var randomNumber= Math.floor((Math.random() * 120) + 19);
 
-    //function to reset game
+    //Random number for gem
+var imgNumber1 = Math.floor((Math.random() * 12) + 1);
+var imgNumber2 = Math.floor((Math.random() * 12) + 1);
+var imgNumber3 = Math.floor((Math.random() * 12) + 1);
+var imgNumber4 = Math.floor((Math.random() * 12) + 1);
+
+var gemTotal = 1;
+
+
+  //function to reset game
+
 function reset() {
-    guessesLeft = 10;
-    userGuessed = [];
-    newLetter();
-    }    
-    
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function (event) {
-    // Determines which key was pressed and change to lowercases
-    var userGuess = event.key.toLowerCase();
-    console.log("Your guess: " + userGuess);
-    
-    // If statments for (win/loss/guesses left)
-        if (userGuess === computerGuess) {
-            wins++;
-            console.log ("You have guessed correctly. You win.");
-            reset ();
-        }    
-    // If user does not match pc guess, decrement guesses left and push lettter to guessed letters.
-        if (userGuess != computerGuess) {
-            guessesLeft--;
-            userGuessed.push(userGuess);
-            console.log("Guess again.")
-        }
-    // If user maxes guess, reset user guesses to 10 and letters guessed
-        if (guessesLeft === 0) {
-            losses++;
-            reset ();
-        }        
+    var wins = 0;
+    var losses = 0;
 
-        //update HTML to show wins, losses, guesses left and letters guessed
-        {
-        document.querySelector('#wins').innerHTML = "Wins: " + wins;
-        document.querySelector('#userguessed').innerHTML = "Your guesses so far: " + userGuessed;
-        document.querySelector('#losses').innerHTML = "Losses: " + losses;
-        document.querySelector('#guessesleft').innerHTML = "Guesses left: " + guessesLeft;
-        }
+    // Randomly math to choose a number from 19-120
+    var randomNumber = 0;
+
+    //Random number for gem
+    var imgNumber1 = 0
+    var imgNumber2 = 0
+    var imgNumber3 = 0
+    var imgNumber4 = 0
+
+    var gemTotal = 1;
+
+}
+   
+$(document).ready(function () { 
+
+function update (params) {
+    $('#gemTotal').text("Gem Total: " + gemTotal);
+    $('#wins').text("Wins: " + wins);
+    $('#losses').text("Losses: " + losses);
+    console.log(gemTotal); 
+}
+//function to run else if statments if equal, or over
+function elsify (params) {
+        if (gemTotal === randomNumber) {
+            console.log("You Win");
+            wins++;
+            reset();
     }
+        else if (gemTotal > randomNumber){
+            console.log("You went over the random number. You Lost.");
+            losses++;
+            reset();
+    }
+}
+//Send random number to text
+    $('#randomNumber').text("Your random number is: " + randomNumber);
+    console.log ("Computer generated random number: " + randomNumber);  
+
+// Creating click on events for each image.
+        $("#img1").on("click", function () {
+            gemTotal += imgNumber1
+            update();
+            elsify();
+            update();
+        // alert("You clicked a crystal! " + imgNumber1);
+        });
+        
+        $("#img2").on("click", function () {
+            gemTotal += imgNumber2;
+            update();
+            elsify();
+            update();
+            // alert("You clicked a crystal! " + imgNumber2);
+        });
+        
+        $("#img3").on("click", function () {
+            gemTotal += imgNumber3;
+            update();
+            elsify();
+            update();
+            // alert("You clicked a crystal! " + imgNumber3);
+        });
+
+        $("#img4").on("click", function () {
+            gemTotal += imgNumber4;
+            update();
+            elsify();
+            update();
+        // alert("You clicked a crystal!" + imgNumber4);
+        });
+
+    });    
