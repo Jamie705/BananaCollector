5fca7c6260756c3f36b73f5c8c926cfa2773230d
myusername <rodriguez.jamie@live.com>
2018-09-08 01:16:49 -0400

    completed html, and css also changed theme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # unit-4game
-Crystal Collector Game
+Banana Collector Game
+
+You will have to guess the answer. 
+
+Here's how the app works:
+
+   * There will be four minions displayed as buttons on the page.
+
+   * There will a random number at the start of the game.
+
+   * When you click on an image, it will add a specific amount of points to the total score. 
+
+   * You win if the total score matches the random number from the beginning of the game.
+
+   * The player loses if their score goes above the random number.
+
+   * The game restarts whenever you win or lose.
+
+//JavaScript & JQuery used
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,60 @@
 // alert("something");
 
-
 // Creating variables to hold the number of wins, losses, guesses left and list of guessed letters.
 var wins = 0;
 var losses = 0;
 
-    // Randomly math to choose a number from 19-120
+//variable for keeping gem total
+var gemTotal = 0;
+
+// Randomly math to choose a number from 19-120
 var randomNumber= Math.floor((Math.random() * 120) + 19);
 
-    //Random number for gem
+//Random number for gems
 var imgNumber1 = Math.floor((Math.random() * 12) + 1);
 var imgNumber2 = Math.floor((Math.random() * 12) + 1);
 var imgNumber3 = Math.floor((Math.random() * 12) + 1);
 var imgNumber4 = Math.floor((Math.random() * 12) + 1);
 
-var gemTotal = 1;
-
-
-  //function to reset game
-
+//function to reset game
 function reset() {
-    var wins = 0;
-    var losses = 0;
-
-    // Randomly math to choose a number from 19-120
-    var randomNumber = 0;
-
-    //Random number for gem
-    var imgNumber1 = 0
-    var imgNumber2 = 0
-    var imgNumber3 = 0
-    var imgNumber4 = 0
-
-    var gemTotal = 1;
-
+    randomNumber = Math.floor((Math.random() * 120) + 19);
+    //Send random number to text
+    $('#randomNumber').text(randomNumber);
+    console.log("Computer generated random number: " + randomNumber);
+    //Random number for gems
+    imgNumber1 = Math.floor((Math.random() * 12) + 1);
+    imgNumber2 = Math.floor((Math.random() * 12) + 1);
+    imgNumber3 = Math.floor((Math.random() * 12) + 1);
+    imgNumber4 = Math.floor((Math.random() * 12) + 1);
+    //reseting gem total
+    gemTotal = 0;
 }
-   
-$(document).ready(function () { 
-
-function update (params) {
-    $('#gemTotal').text("Gem Total: " + gemTotal);
-    $('#wins').text("Wins: " + wins);
-    $('#losses').text("Losses: " + losses);
-    console.log(gemTotal); 
-}
-//function to run else if statments if equal, or over
-function elsify (params) {
-        if (gemTotal === randomNumber) {
-            console.log("You Win");
-            wins++;
-            reset();
+    //update score
+    function update (params) {
+        $('#gemTotal').text("Gem Total: " + gemTotal);
+        $('#wins').text("Wins: " + wins);
+        $('#losses').text("Losses: " + losses);
+        console.log(gemTotal); 
     }
-        else if (gemTotal > randomNumber){
-            console.log("You went over the random number. You Lost.");
-            losses++;
-            reset();
+    //function to run else if statments if equal, or over
+    function elsify (params) {
+            if (gemTotal === randomNumber) {
+                console.log("You Win");
+                wins++;
+                reset()
+            }
+            else if (gemTotal > randomNumber){
+                console.log("You went over the random number. You Lost.");
+                losses++;
+                reset()
+            }
     }
-}
 //Send random number to text
-    $('#randomNumber').text("Your random number is: " + randomNumber);
-    console.log ("Computer generated random number: " + randomNumber);  
+    $('#randomNumber').text(randomNumber);
+    console.log("Computer generated random number: " + randomNumber);
 
-// Creating click on events for each image.
+        // Creating click on events for each image.
         $("#img1").on("click", function () {
             gemTotal += imgNumber1
             update();
@@ -93,5 +86,3 @@ function elsify (params) {
             update();
         // alert("You clicked a crystal!" + imgNumber4);
         });
-
-    });    
